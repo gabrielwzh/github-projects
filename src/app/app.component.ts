1bef59e55b42d3539cf67b553a21e782de571e58
unknown <gabrielwonggg@gmail.com>
2020-03-04 18:32:13 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { forkJoin } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'github-projects';
+
+  constructor(private http: HttpClient) {}
+
+  username = '';
+  projects: any;
+  count: number;
+  getMarkdown: any;
+  myHTML: string;
+
+
+  submit() {
+    let array = [];
+    let lastPage;
+    const observablesList = [];
+    console.log(this.username);
+
+    this.http.get('https://api.github.com/users/' + this.username + '/repos?page=1&per_page=50', {observe: 'response'})
+    .subscribe(resp => {
+    if (resp.headers.get('Link') === null) {
+      lastPage = 1;
+    } else {
+      lastPage = resp.headers.get('Link').split('page=')[3].split('&')[0];
+    }
+
+    console.log('last page: ' + lastPage);
+
+    for (let i = 1; i < lastPage + 1; i++) {
+        observablesList.push(this.http.get('https://api.github.com/users/' + this.username + '/repos?page=' + i + '&per_page=50'));
+    }
+
+    forkJoin(observablesList).subscribe(results => {
+       // tslint:disable-next-line: prefer-for-of
+       for (let i = 0; i < results.length; i++) {
+       array = array.concat(results[i]);
+       }
+       this.projects = array;
+       this.count = array.length;
+       console.log(this.projects);
+      });
+    });
+
+    this.myHTML = '';
+  }
+
+  selectProject(proj) {
+    console.log(proj);
+    this.myHTML = '';
+    this.getMarkdown = this.http.get('https://raw.githubusercontent.com/' + this.username + '/' + proj + '/master/README.md',
+    {responseType: 'text'});
+    this.getMarkdown.subscribe( data => { this.myHTML = data; });
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MarkdownModule } from 'ngx-markdown';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MarkdownModule.forRoot(),
+    NgxPaginationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
